04345c36462fe4ff0f44f869c40eceed7eb77737
Matthias Glaub <magl@magl.net>
2014-02-25 00:12:58 +0100

    cleaning up test scripts * remove composer.lock for better testing * extending tests for php 5.4 and 5.3

--- test/Bootstrap.php
+++ test/Bootstrap.php
@@ -43,6 +43,10 @@ class Bootstrap
 		static::$serviceManager = $app->getServiceManager();
 	}
 
+	/**
+	 * 
+	 * @return \Zend\ServiceManager\ServiceManager
+	 */
 	public static function getServiceManager()
 	{
 		return static::$serviceManager;
@@ -50,30 +54,15 @@ class Bootstrap
 
 	protected static function initAutoloader()
 	{
-		//$vendorPath = static::findParentPath('vendor');
 		$vendorPath = static::findParentPath('vendor');
-
-		$zf2Path = getenv('ZF2_PATH');
-		if (!$zf2Path) {
-			if (defined('ZF2_PATH')) {
-				$zf2Path = ZF2_PATH;
-			} elseif (is_dir($vendorPath . '/ZF2/library')) {
-				$zf2Path = $vendorPath . '/ZF2/library';
-			} elseif (is_dir($vendorPath . '/zendframework/zendframework/library')) {
-				$zf2Path = $vendorPath . '/zendframework/zendframework/library';
-			}
-		}
-
-		if (!$zf2Path) {
-			throw new RuntimeException('Unable to load ZF2. Run `php composer.phar install` or define a ZF2_PATH environment variable.');
-		}
 
 		// Composer autoloading
 		if (file_exists($vendorPath . '/autoload.php')) {
-			$loader = include $vendorPath . '/autoload.php';
+			include $vendorPath . '/autoload.php';
+		} else {
+			throw new RuntimeException('Unable to find composer autoloader. Run `php composer.phar install`.'); 
 		}
 
-		include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
 		AutoloaderFactory::factory(array(
 			'Zend\Loader\StandardAutoloader' => array(
 				'autoregister_zf' => true,
@@ -90,8 +79,9 @@ class Bootstrap
 		$previousDir = '.';
 		while (!is_dir($dir . '/' . $path)) {
 			$dir = dirname($dir);
-			if ($previousDir === $dir)
+			if ($previousDir === $dir) {
 				return false;
+			}
 			$previousDir = $dir;
 		}
 		return $dir . '/' . $path;
--- test/MaglMarkdownTest/ModuleTest.php
+++ test/MaglMarkdownTest/ModuleTest.php
@@ -25,39 +25,51 @@ class ModuleTest extends AbstractControllerTestCase
 	 */
 	private $instance;
 
-    public function setUp()
-    {
-        $this->instance = new Module();
-    }
+	public function setUp()
+	{
+		$this->instance = new Module();
+	}
 
-    public function testInstance()
-    {
-        $this->assertInstanceOf('MaglMarkdown\Module', $this->instance);
-    }
+	public function testInstance()
+	{
+		$this->assertInstanceOf('MaglMarkdown\Module', $this->instance);
+	}
 
-    public function testGetViewHelperConfig()
-    {
-        $config = $this->instance->getViewHelperConfig();
+	public function testGetViewHelperConfig()
+	{
+		$config = $this->instance->getViewHelperConfig();
 
-        $this->assertTrue(array_key_exists('markdown', $config['invokables']));
-    }
+		$this->assertTrue(array_key_exists('markdown', $config['invokables']));
+	}
 
 	public function testGetAutoloaderConfig()
-    {
-        $config = $this->instance->getAutoloaderConfig();
+	{
+		$config = $this->instance->getAutoloaderConfig();
 
-        $this->assertTrue(array_key_exists('MaglMarkdown', $config['Zend\Loader\StandardAutoloader']['namespaces']));
-    }
+		$this->assertTrue(array_key_exists('MaglMarkdown', $config['Zend\Loader\StandardAutoloader']['namespaces']));
+	}
+
 	public function testGetServiceConfig()
-    {
-        $config = $this->instance->getServiceConfig();
+	{
+		$config = $this->instance->getServiceConfig();
 
-        $this->assertTrue(array_key_exists('MaglMarkdown\Adapter\ErusevParsedownAdapter', $config['invokables']));
-        $this->assertTrue(array_key_exists('MaglMarkdown\Adapter\MichelfPHPMarkdownAdapter', $config['invokables']));
-        $this->assertTrue(array_key_exists('MaglMarkdown\Adapter\MichelfPHPMarkdownExtraAdapter', $config['invokables']));
-        
+		$this->assertTrue(array_key_exists('MaglMarkdown\Adapter\ErusevParsedownAdapter', $config['invokables']));
+		$this->assertTrue(array_key_exists('MaglMarkdown\Adapter\MichelfPHPMarkdownAdapter', $config['invokables']));
+		$this->assertTrue(array_key_exists('MaglMarkdown\Adapter\MichelfPHPMarkdownExtraAdapter', $config['invokables']));
+
 		$this->assertTrue(array_key_exists('MaglMarkdown\MarkdownAdapter', $config['aliases']));
-    }
+	}
 
-	
+	public function testGetViewHelper()
+	{
+		$serviceManager = Bootstrap::getServiceManager();
+		
+		/* @var $view \Zend\View\HelperPluginManager */
+        $view         = $serviceManager->get('ViewHelperManager');
+		
+		$markdown = $view->get('markdown');
+		$this->assertInstanceOf('MaglMarkdown\View\Helper\Markdown', $markdown);
+		$this->assertInstanceOf('Zend\View\Helper\HelperInterface', $markdown);
+	}
+
 }
